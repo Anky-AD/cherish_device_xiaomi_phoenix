919e96a37162fa8dc67032c98adb8741611c6476
redcliff-op <devyashjangid@gmail.com>
2021-05-25 00:53:18 +0530

    phoenix: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+rm -rf hardware/qcom/audio/adsprpcd
+rm -rf hardware/qcom-caf/sm8150/audio
+git clone https://github.com/MrJarvis698/android_hardware_qcom_audio -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/display
+git clone https://github.com/MrJarvis698/platform_hardware_qcom_display -b 11-caf-sm8150-octavi hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media
+git clone https://github.com/MrJarvis698/android_hardware_qcom_media -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-18.0 hardware/xiaomi
+rm -rf system/bt 
+git clone https://github.com/ShapeShiftOS/android_system_bt.git system/bt
+rm -rf vendor/qcom/opensource/commonsys/system/bt
+git clone https://github.com/ShapeShiftOS/android_vendor_qcom_opensource_system_bt.git vendor/qcom/opensource/commonsys/system/bt
+rm -rf vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+git clone https://github.com/ShapeShiftOS/android_vendor_qcom_opensource_packages_apps_Bluetooth.git vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+rm -rf packages/apps/Bluetooth
+git clone https://github.com/ShapeShiftOS/android_packages_apps_Bluetooth.git packages/apps/Bluetooth
+git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+
